fda64bfe20114c37a6f78840ab0729ef4a9cea7a
Max Montgomery <mmontgomery14@Max-Montgomery-P3JWDXCGGJ.local>
2022-12-15 13:47:00 -0500

    convert to typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,19 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-const name = 'Max Montgomery';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'Teacher X'
+export const siteTitle = 'Teacher X'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,8 +25,8 @@ export default function Layout({ children, home }) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -33,8 +39,8 @@ export default function Layout({ children, home }) {
               src="/images/smiley.jpg"
               className={utilStyles.borderCircle}
               height={144}
-              width={144}
-              alt=""
+              width={164}
+              alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
@@ -46,8 +52,8 @@ export default function Layout({ children, home }) {
                 src="/images/smiley.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
-                width={108}
-                alt=""
+                width={118}
+                alt={name}
               />
             </Link>
             <h2 className={utilStyles.headingLg}>
@@ -65,5 +71,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  );
+  )
 }
